04b6edb92f2d2e0900edcd3500ed63df96da75ed
unknownpgr <unknownpgr@gmail.com>
2021-07-13 07:24:41 +0000

    Implement kakao login

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const express = require("express")
 const path = require('path')
+const passport = require('passport')
+const axios = require('axios')
+const KakaoStrategy = require('passport-kakao').Strategy
+
+const {
+    clientID,
+    callbackURL
+} = require('./secrets')
+
+passport.use('kakao',
+    new KakaoStrategy({ clientID, callbackURL }, (accessToken, refreshToken, profile, done) => {
+        console.log('Access', accessToken)
+        console.log('Refresh', refreshToken)
+        console.log('Profile', profile)
+        done(null, profile)
+    })
+)
 
 const app = express()
 
@@ -8,5 +25,10 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => {
     res.redirect('/index.html')
 })
+
+app.get('/oauth/kakao/login', passport.authenticate('kakao'))
+app.get('/oauth/kakao/callback', passport.authenticate('kakao', { session: false, }), (req, res) => {
+    res.redirect('/');
+});
 
 app.listen(80)
